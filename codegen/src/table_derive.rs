7b10466b95ca2884444ecd054f35446e4850c967
Daogang Tang <daogangtang@live.com>
2018-09-29 15:26:57 +0800

    make rustorm_codegen don't depend on the alias of dao, using the original name rustorm_dao.
    Signed-off-by: Daogang Tang <daogangtang@live.com>

--- codegen/src/table_derive.rs
+++ codegen/src/table_derive.rs
@@ -6,8 +6,8 @@ pub fn impl_to_table_name(ast: &syn::MacroInput) -> quote::Tokens {
     quote! {
         impl ToTableName for  #name {
 
-            fn to_table_name() -> dao::TableName {
-                dao::TableName{
+            fn to_table_name() -> rustorm_dao::TableName {
+                rustorm_dao::TableName{
                     name: stringify!(#name).to_lowercase().into(),
                     schema: None,
                     alias: None,
