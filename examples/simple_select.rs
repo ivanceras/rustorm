3e857645b537d53da6ed52f6d0e348afe7db0cd5
Jovansonlee Cesar <ivanceras@gmail.com>
2018-12-11 14:24:21 +0800

    Add simple select usage without a struct

---
+++ examples/simple_select.rs
@@ -0,0 +1,27 @@
+#[macro_use]
+extern crate rustorm_codegen;
+extern crate rustorm_dao as dao;
+extern crate rustorm_dao;
+extern crate rustorm;
+use rustorm::TableName;
+use rustorm_dao::ToColumnNames;
+use rustorm_dao::ToTableName;
+use rustorm_dao::{FromDao, ToDao};
+use rustorm::Pool;
+use rustorm::DbError;
+use rustorm::Rows;
+
+
+fn main(){
+    let db_url = "postgres://postgres:p0stgr3s@localhost/sakila";
+    let mut pool = Pool::new();
+    let dm = pool.dm(db_url).unwrap();
+    let sql = "SELECT * FROM actor LIMIT 10";
+    let actors: Result<Rows, DbError> = dm.execute_sql_with_return(sql, &[]);
+    println!("Actor: {:#?}", actors);
+    let actors = actors.unwrap();
+    assert_eq!(actors.iter().len(), 10);
+    for actor in actors.iter() {
+        println!("actor: {:?}", actor);
+    }
+}
