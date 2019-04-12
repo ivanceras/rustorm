e442255f8abcecea2a601a191a379ad8b436dd4d
Jovansonlee Cesar <ivanceras@gmail.com>
2019-04-12 16:10:58 +0800

    Skip bootstrap in tests

--- test.sh
+++ test.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -v
 
-if . ./bootstrap-test.sh ; then
-    cargo test --features "with-postgres with-sqlite"
-    cargo test --features "with-postgres"
-    cargo test --features "with-sqlite"
-    cargo test --all-features
-fi
+cargo test --features "with-postgres with-sqlite"
+cargo test --features "with-postgres"
+cargo test --features "with-sqlite"
+cargo test --all-features
