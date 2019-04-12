4b7a9b4158b0361b74f0c0ade1cdcfdc99d5a35b
Jovansonlee Cesar <ivanceras@gmail.com>
2019-04-12 17:01:23 +0800

    Add the 2 missing tables used in the test

--- import-sakila-pg.sh
+++ import-sakila-pg.sh
@@ -2,5 +2,9 @@ psql -U postgres -h localhost -c 'DROP DATABASE sakila;'
 psql -U postgres -h localhost -c 'CREATE DATABASE sakila;'
 psql -U postgres -h localhost -d sakila -f ../sakila/postgres-sakila-db/postgres-sakila-schema.sql
 psql -U postgres -h localhost -d sakila -f ../sakila/postgres-sakila-db/postgres-sakila-data.sql
+
+psql -U postgres -h localhost -d sakila -f ../sakila/postgres-sakila-db/add_users_table.sql
+psql -U postgres -h localhost -d sakila -f ../sakila/postgres-sakila-db/add_composite_film_actor_award_foreign_key.sql
+
 psql -U postgres -h localhost -c "ALTER USER postgres WITH PASSWORD 'p0stgr3s';"
 
